42874896f5dd3a28a8fbcc88950295b6ac1eae28
Max Altenhofen <maxaltenhofen@gmail.com>
2026-04-12 15:48:48 -0500

    Made some structural changes to sv file. Saved old one for reference with yosys

--- src/BarrelShifter_old.sv
+++ src/BarrelShifter_old.sv
@@ -13,32 +13,50 @@ module BarrelShifter #(
 
 	logic [DATA_W-1:0] stage [SHAMT_W:0];
 
-	always @(*) begin
+	always_comb begin
 		stage[0] = data_in;
-		for (int i = 0; i < SHAMT_W; i++) begin
-			if (shamt[i]) begin
-				for (int j = 0; j < DATA_W; j++) begin
-					case (mode)
-						2'b00: begin // logic left
+		case (mode)
+			2'b00: begin // logic left
+				for (int i = 0; i < SHAMT_W; i++) begin
+					if (shamt[i]) begin
+						for (int j = 0; j < DATA_W; j++) begin
 							if (j >= (1 << i)) stage[i+1][j] = stage[i][j - (1<<i)];
 							else stage[i+1][j] = 1'b0;
 						end
-						2'b01: begin // logic right
+					end else stage[i+1] = stage[i];
+				end
+			end
+			2'b01: begin // logic right
+				for (int i = 0; i < SHAMT_W; i++) begin
+					if (shamt[i]) begin
+						for (int j = 0; j < DATA_W; j++) begin
 							if (j < DATA_W - (1 << i)) stage[i+1][j] = stage[i][j + (1<<i)];
 							else stage[i+1][j] = 1'b0;
 						end
-						2'b10: begin // arithmetic right
+					end else stage[i+1] = stage[i];
+				end
+			end
+			2'b10: begin // arithmetic right
+				for (int i = 0; i < SHAMT_W; i++) begin
+					if (shamt[i]) begin
+						for (int j = 0; j < DATA_W; j++) begin
 							if (j < DATA_W - (1 << i)) stage[i+1][j] = stage[i][j + (1<<i)];
 							else stage[i+1][j] = stage[i][DATA_W-1];
 						end
-						default: begin // Default to logic left
+					end else stage[i+1] = stage[i];
+				end
+			end
+			default: begin // Default to logic left
+				for (int i = 0; i < SHAMT_W; i++) begin
+					if (shamt[i]) begin
+						for (int j = 0; j < DATA_W; j++) begin
 							if (j >= (1 << i)) stage[i+1][j] = stage[i][j - (1<<i)];
 							else stage[i+1][j] = 1'b0;
 						end
-					endcase
+					end else stage[i+1] = stage[i];
 				end
-			end else stage[i+1] = stage[i];
-		end
+			end
+		endcase
 		data_out = stage[SHAMT_W]; 
 	end
 endmodule
